3f5fd25807837c2a96ad93f5ee5385ab2bdc9d1e
thaophamhetxat <thaophamhetxat@gmail.com>
2021-09-17 15:01:50 +0700

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Pet} from "./pet";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'md5-HienThiThongTinPer';
+export class AppComponent implements OnInit {
+  pet: Pet = {
+    name: 'puppie',
+    image: 'http://yourdost-blog-images.s3-ap-southeast-1.amazonaws.com/wp-content/uploads/2016/01/03165939/Dogs.jpg'
+  };
+  // title = 'md5-HienThiThongTinPer';
+
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
 }
---
+++ src/app/pet.ts
@@ -0,0 +1,4 @@
+export interface Pet {
+  name?:string;  // ?: là khai báo thuộc tính optional trong interface
+  image?:string;
+}
